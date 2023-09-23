a81432b53a0be07cdda80f5ec09d0f7962c3fdb5
chiageng <chiageng12@gmail.com>
2023-09-23 14:55:38 +0800

    change route

--- routes/general.js
+++ routes/general.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/test", test)
 router.post("/lazy-developer", getNextProbableWords)
-router.post("/greedy-monkey", greedyMonkey)
+router.post("/greedymonkey", greedyMonkey)
 router.post("/digital-colony", greedyMonkey)
 
 
